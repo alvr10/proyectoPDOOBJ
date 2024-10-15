90b4cd676a88b1fdddb11f3d075b5f95081f8dd7
Alvaro Rios <144827959+alvr10@users.noreply.github.com>
2024-10-15 11:47:10 +0200

    Add files via upload

---
+++ MyBlackjack/src/myblackjack/Player.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package myblackjack;
+
+/**
+ *
+ * @author alvar
+ */
+public class Player {
+    private Hand hand;
+    
+    
+}
---
+++ MyBlackjack/src/myblackjack/Shoe.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package myblackjack;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author alvar
+ */
+public class Shoe {
+    private final List<Deck> shoe;
+    private final int numberOfDecks;
+    
+    public Shoe() {
+        this.numberOfDecks = 6;
+        shoe = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            shoe.add(new Deck());
+        }
+    }
+    
+    public Shoe(int numberOfDecks) {
+        this.numberOfDecks = numberOfDecks;
+        shoe = new ArrayList<>();
+        for (int i = 0; i < numberOfDecks; i++) {
+            shoe.add(new Deck());
+        }
+    }
+    
+    public List<Deck> getShoe() {
+        return shoe;
+    }
+    
+    public Card getTopCardFromTopDeck() {
+        for (Deck deck : shoe) {
+            if (!deck.getCards().isEmpty()) {
+                return deck.getDeckTopCard();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Shoe{");
+        sb.append("shoe=").append(shoe);
+        sb.append(", numberOfDecks=").append(numberOfDecks);
+        sb.append('}');
+        return sb.toString();
+    }
+}
